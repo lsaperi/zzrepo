56ef08441b292a101024155ec15a029e6399928a
lsaperi <lsaperi>
2025-05-27 08:43:19 -0300

    test; TaskID: task1

---
+++ TABLEs/Creation-Scripts/products.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `products` (
+  `idproducts` int NOT NULL,
+  `productscol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idproducts`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
