8095fa765381d5a794c0da608f4aee39f0d8988f
lsaperi <lsaperi>
2025-05-27 10:53:32 -0300

    fdsfds; TaskID: columncomment

--- TABLEs/Creation-Scripts/persons.sql
+++ TABLEs/Creation-Scripts/persons.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `persons` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL COMMENT 'name of th eperson',
   `age` int DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='testing table'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
