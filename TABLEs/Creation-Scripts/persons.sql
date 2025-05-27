e1e314cf7e1c296821fabcfc66e337b162845fc0
lsaperi <lsaperi>
2025-05-27 10:39:26 -0300

    fdsfds; TaskID: comment

--- TABLEs/Creation-Scripts/persons.sql
+++ TABLEs/Creation-Scripts/persons.sql
@@ -4,4 +4,4 @@ CREATE TABLE `persons` (
   `age` int DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='testing table'
